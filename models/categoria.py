f6ae5311f39631980687dff345eb3f32518ea330
disoa <@diogosm>
2024-03-30 05:39:18 -0300

    added ordem para Categoria()

--- models/categoria.py
+++ models/categoria.py
@@ -6,9 +6,16 @@ class Categoria(db.Model):
     questionario_id = db.Column(db.Integer, db.ForeignKey('questionario.id'))
     date_created = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
     date_updated = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    ordem = db.Column(db.Integer, nullable=True)
 
     questoes = db.relationship('Questao', backref='categoria', lazy=True)
 
     def __init__(self, nome, questionario_id):
         self.nome = nome
         self.questionario_id = questionario_id
+
+    @classmethod
+    def categoria_ordem(cls, nome, questionario_id, ordem):
+        categoria = cls(nome, questionario_id)
+        categoria.ordem = ordem
+        return categoria
