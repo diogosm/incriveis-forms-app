b173d9482810dbd055249f7048d7568d8a0426f6
disoa <@diogosm>
2024-03-30 06:32:43 -0300

    added 404 e 500 internal error pages

--- main.py
+++ main.py
@@ -53,6 +53,15 @@ def load_user(user_id):
     return dao.get_user(user_id)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 from routes import rotas
 
 # Register the routes
